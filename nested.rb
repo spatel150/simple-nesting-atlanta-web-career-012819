8745552a4c2389e71a22c7a2d22e85a5b6ce0e42
Samidh Patel <spatel13456@gmail.com>
2019-01-15 05:39:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+      
+   return programmer_hash[:alan_kay][:known_for] = "GUI"  
 end
 
 def adding_to_dennis
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  return programmer_hash
 end
